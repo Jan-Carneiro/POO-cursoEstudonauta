295d22c488057b88cec8114358ffb7ee64ec2fc4
Jan Carneiro Batista <jancarneiro3@gmail.com>
2023-11-29 11:11:57 -0300

    atualização poo

--- aula04/index.php
+++ aula04/index.php
@@ -9,7 +9,7 @@
     <?php 
         require_once 'Caneta.php';
 
-        $c1 = new Caneta();
+        //$c1 = new Caneta();
         //print_r ($c1)
 
         //$c1 -> setModelo ("BIC");
@@ -20,9 +20,17 @@
         //$c1 -> ponta = 0.5;
         //print_r ($c1);
 
-        $c1 -> setModelo ("BIC");
-        $c1 -> setPonta (0.5);
-        print "Eu tenho uma caneta {$c1 -> getModelo()} de ponta {$c1 -> getPonta()}";
+        //$c1 -> setModelo ("BIC");
+        //$c1 -> setPonta (0.5);
+        //print "Eu tenho uma caneta {$c1 -> getModelo()} de ponta {$c1 -> getPonta()}";
+
+        $c1 = new Caneta ("BIC","Azul",0.5);
+        $c2 = new Caneta ("NIC","Preta",0.8);
+        $c3 = new Caneta ("PIC", "Verde", 0.6);
+        print_r($c1);
+        print_r($c2);
+        print_r($c3);
+
     ?>
 </body>
 </html>
